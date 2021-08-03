783bd7857a692a63ea50f6bdc0c8270a1e3b6835
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-08-03 14:02:54 -0300

    Ajustando matrizes de tuplas

--- checkpoint.py
+++ checkpoint.py
@@ -1,10 +1,32 @@
-oficinas = ("Criar e contar histórias", "Teatro: Luz, Câmera e Ação", "A língua de sinais", "Expressão artística", "Soletrando", "Leitura dramática", "O corpo fala", "O mundo da imaginação", "Leitura dinâmica", "Criando e recriando com emojis")
+oficinasMatutino = (("Criar e contar histórias", "A língua de sinais"),
+("Criar e contar histórias", "Teatro: Luz, Câmera e Ação", "A língua de sinais"),
+("Teatro: Luz, Câmera e Ação", "A língua de sinais", "Expressão Artística"),
+("A língua de sinais", "Expressão Artística", "Soletrando"))
+
+oficinasVespertino = (("O mundo da imaginação", "Criando Emojis"),
+("O corpo fala"), ("Leitura dramática"), ("Leitura dinâmica"))
+
+
 series = ("2ª serie", "3ª serie", "4ª serie", "5ª serie")
 # Alunos
 rmAlunos = []
 serieAlunos = []
+nomeAlunos = []
+
 # Variável de controle
 cadastro = True
+
+def periodo(lista):
+    periodo = input("Digite o período do aluno: ").strip()[0]
+    while (periodo not in "MVT"):
+        print("Período inválido... (Matutino ou Vespertino)")
+        periodo = input("Digite o período do aluno: ").strip()[0]
+        if (periodo == "M"):
+            periodo = "Matutino"
+            lista.append(periodo)
+        elif (periodo == "V" or periodo == "T"):
+            periodo = "Vespertino"
+            lista.append(periodo)
 
 def menu():
     print("Evento Literário -- Colégio Nova Esperança")
@@ -54,13 +76,21 @@ while True:
                     print("Série inválida!")
                     serie = int(input("Digite a serie do aluno: ").strip()[0])
                 serieAlunos.append(series[serie-2])
+                nome = input("Digite o nome do aluno: ").strip().capitalize()
+                nomeAlunos.append(nome)
+                
     # Inscrição
     elif (selecao == 2):
         ...
     # Listagem
     elif (selecao == 3):
+        print(input("""Menu Listar inscrições
+1 - Listar por aluno (ordem alfabética de nome)
+2 - Listar por oficina (ordem alfabética)"""))
         ...
     else:
         print(rmAlunos)
         print(serieAlunos)
+        print(nomeAlunos)
+        print(periodoAlunos)
         break
